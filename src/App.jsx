57b30ab10462fe62453181144376d08b16891088
UchmannY <ouchenna63@yahoo.com>
2025-08-27 10:08:23 +0100

    Fetch Weather Details by Selected City Cordinates

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import CitySearch from './citySearch';
 function App() {
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
         </Toolbar>
       </AppBar>
     </Box>
+    <CitySearch />
     </Container>
     </>
   )
--- src/citySearch.jsx
+++ src/citySearch.jsx
@@ -6,10 +6,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment'
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 function CitySearch() {
 
     const [cityOptions, setCityOptions] = useState([]);
     const [text, setText] = useState('');
+    const [selectedCity, setSelectedCity] = useState('')
+    const [weatherDetails, setWeatherDetails] = useState(null);
+    
     React.useEffect (() => {
         const cityTimeoutCall = setTimeout(() => { 
             ( async () => {
@@ -32,6 +36,29 @@ function CitySearch() {
         )()},1000)
         return () => clearTimeout(cityTimeoutCall);
     },[text])
+
+    const fetchWeatherByCity = async (city) => {
+
+        if (city) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.latitude}&lon=${city.longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`);
+            const data = await response.json();
+            console.log("Weather Details",data);
+            const weatherInfo = {
+                Description: data.weather[0].description,
+                Temperature: data.main.temp,
+                Wind_Speed: data.wind.speed,
+                Humidity : data.main.humidity,
+                iconUrl: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            }
+
+            setWeatherDetails(weatherInfo);
+        }
+        catch (err) {
+            console.error("Error fetching weather:", err)
+        }
+    }
+    } 
     return (
         <>
         <Autocomplete
@@ -40,6 +67,30 @@ function CitySearch() {
         
         inputValue={text}
         onInputChange={(e, newInputValue) => setText(newInputValue)}
+        value={selectedCity}
+        onChange={(e, newInputValue) => {
+          if (newInputValue !== null) setSelectedCity(newInputValue);
+        }}
+        sx={{ width: 400, margin:'auto', marginBottom:10}}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search City"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                {params.InputProps.endAdornment}
+                <InputAdornment position="end">
+                  <IconButton onClick={() => fetchWeatherByCity(selectedCity || text)}>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+                </>
+              )
+            }}
+          />
+        )}
       />
       </>
       
