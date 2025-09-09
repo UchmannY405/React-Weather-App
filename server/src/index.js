f83b6a325e46cc6e78f732c219d9372caddcf8d0
UchmannY <ouchenna63@yahoo.com>
2025-09-09 17:39:05 +0100

    Refactor(Index.js): Wired errorHandlerMiddleware to Index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 const connectDB = require('../db/connect');
 const express = require('express');
 const app = express();
@@ -6,6 +7,7 @@ const authRoute = require('../routes/auth')
 const userRoute = require('../routes/users')
 const notFound = require('../middleware/notFound');
 const dashAuth = require('../middleware/dashAuth')
+const errorHandlerMiddleware = require('../middleware/error-handler')
 
 const PORT = process.env.PORT || 5050
 
@@ -14,6 +16,9 @@ app.use(express.json());
 app.use('/api/v1/auth',authRoute);
 app.use('/api/v1/users',dashAuth,userRoute);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+
 const startServer = async ()=>{
     try
     {
