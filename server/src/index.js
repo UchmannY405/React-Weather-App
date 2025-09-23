dfba95b1b387e672a7c6c64a2115f992109afc7f
UchmannY <ouchenna63@yahoo.com>
2025-09-23 21:36:39 +0100

    Fix(cors): Allow Vercel Frontend Domain in CORS Configuration

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://react-weather-app-sand-one.vercel.app"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
